136b3b0f7844e29859b4c68c958591800bbb10e5
hasannasrul <hasanali242424@gmail.com>
2023-05-01 22:53:58 +0530

    added stop

--- first-instance/main.tf
+++ first-instance/main.tf
@@ -8,6 +8,8 @@ resource "aws_instance" "first-tf-instance" {
     tags = {
         Name: "TF-instance"
     }
+    # to stop instance use stp in below line and to start use start in below line
+    instance_initiated_shutdown_behavior = "stop"
     key_name = "python-devops"
     user_data = <<-EOF
                 #!/bin/bash
@@ -15,5 +17,10 @@ resource "aws_instance" "first-tf-instance" {
                 sudo apt-get install docker.io -y
                 sudo systemctl start docker
                 sudo systemctl enable docker
+                sudo apt-get install nginx -y
                 EOF
+}
+
+output ip {
+  value       = aws_instance.first-tf-instance.public_ip
 }
